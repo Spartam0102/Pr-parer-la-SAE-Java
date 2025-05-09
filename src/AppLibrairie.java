ec8d02873b60043cb7cdb1354ee72fba86003a1b
trojnar-paul <paul.trojnar@etu.univ-orleans.fr>
2025-05-09 12:19:47 +0200

    modification app

--- src/AppLibrairie.java
+++ src/AppLibrairie.java
@@ -1,48 +1,47 @@
-
 public class AppLibrairie {
     private Entreprise entreprise;
 
-    private boolean quitter;
+    private boolean quitterApp = false;
 
     public AppLibrairie(Entreprise entreprise) {
         this.entreprise = entreprise;
-        this.quitter = false;
     }
 
     public void run() {
         bienvenue();
-        while (!quitter) {
-            menu();
+        while (!quitterApp) {
+            menuPrincipal();
         }
         auRevoir();
     }
 
-    public void menu() {
-        boolean passerSuivant = false;
-        while (!passerSuivant) {
+    public void menuPrincipal() {
+        boolean menuActif = true;
+        while (menuActif && !quitterApp) {
             System.out.println("+-------------------------+");
             System.out.println("|  Menu principal         |");
             System.out.println("+-------------------------+");
             System.out.println("| Q: Quitter              |");
             System.out.println("| C: Connexion            |");
             System.out.println("+-------------------------+");
+
             String commande = lireCommande();
 
             if (commande.equals("q")) {
-                quitter = true;
-                passerSuivant = true;
+                quitterApp = true;
+                menuActif = false;
             } else if (commande.equals("c")) {
-                connexion();
-                passerSuivant = true;
+                menuConnexion();
+                menuActif = false; // revient au menu principal après connexion
             } else {
                 System.out.println("Commande invalide.");
             }
         }
     }
 
-    public void connexion() {
-        boolean passerSuivant = false;
-        while (!passerSuivant && !quitter) {
+    public void menuConnexion() {
+        boolean menu2 = false;
+        while (!menu2 && !quitterApp) {
             System.out.println("+-------------------------+");
             System.out.println("|  Connexion              |");
             System.out.println("+-------------------------+");
@@ -51,60 +50,106 @@ public class AppLibrairie {
             System.out.println("| V: Vendeur              |");
             System.out.println("| A: Administrateur       |");
             System.out.println("+-------------------------+");
+
             String commande = lireCommande();
 
             if (commande.equals("r")) {
-                passerSuivant = true;
+                menu2 = true;
             } else if (commande.equals("c")) {
-                client();
+                menuClient();
             } else if (commande.equals("v")) {
-                vendeur();
+                menuVendeur();
             } else if (commande.equals("a")) {
-                administrateur();
+                menuAdministrateur();
             } else {
                 System.out.println("Commande invalide.");
             }
         }
     }
 
-    public void client() {
-        menuRole("Client");
-    }
-
-    public void vendeur() {
-        menuRole("Vendeur");
-    }
-
-    public void administrateur() {
-        menuRole("Administrateur");
-    }
-
-    public void menuRole(String role) {
-        boolean commandeFaite = false;
-        while (!commandeFaite && !quitter) {
+    public void menuClient() {
+        boolean menu3 = false;
+        while (!menu3 && !quitterApp) {
             System.out.println("+-------------------------+");
-            System.out.println("|  " + role);
+            System.out.println("| Client                  |");
             System.out.println("+-------------------------+");
             System.out.println("| Q: Quitter              |");
-            if (role.equals("Administrateur")){
-                System.out.println("| a: afficher les magasins             |");
-
-            }
-            System.out.println("| P: Menu principal       |");
+            System.out.println("| A: Afficher magasins    |");
+            System.out.println("| P: Menu précédent       |");
             System.out.println("+-------------------------+");
+
             String commande = lireCommande();
 
             if (commande.equals("q")) {
-                quitter = true;
-                commandeFaite = true;
-            }
-            if (commande.equals("a")) {
-                quitter = true;
-                commandeFaite = true;
+                quitterApp = true;
+                menu3 = true;
+            } else if (commande.equals("a")) {
                 System.out.println(entreprise.toString());
-
             } else if (commande.equals("p")) {
-                commandeFaite = true; // retour au menu principal
+                menu3 = true;
+            } else {
+                System.out.println("Commande invalide.");
+            }
+        }
+    }
+
+    public void menuVendeur() {
+        boolean menu3 = false;
+        while (!menu3 && !quitterApp) {
+            System.out.println("+-------------------------+");
+            System.out.println("| Vendeur                 |");
+            System.out.println("+-------------------------+");
+            System.out.println("| Q: Quitter              |");
+            System.out.println("| A: Afficher magasins    |");
+            System.out.println("| P: Menu précédent       |");
+            System.out.println("+-------------------------+");
+
+            String commande = lireCommande();
+
+            if (commande.equals("q")) {
+                quitterApp = true;
+                menu3 = true;
+            } else if (commande.equals("a")) {
+                System.out.println(entreprise.toString());
+            } else if (commande.equals("p")) {
+                menu3 = true;
+            } else {
+                System.out.println("Commande invalide.");
+            }
+        }
+    }
+
+    public void menuAdministrateur() {
+        boolean menu3 = false;
+        while (!menu3 && !quitterApp) {
+            System.out.println("+-------------------------+");
+            System.out.println("| Administrateur          |");
+            System.out.println("+-------------------------+");
+            System.out.println("| Q: Quitter              |");
+            System.out.println("| A: Afficher magasins    |");
+            System.out.println("| J: ajpouter magasins    |");
+            System.out.println("| P: Menu précédent       |");
+            System.out.println("+-------------------------+");
+
+            String commande = lireCommande();
+
+            if (commande.equals("q")) {
+                quitterApp = true;
+                menu3 = true;
+            } else if (commande.equals("a")) {
+                System.out.println(entreprise.toString());
+            } else if (commande.equals("j")) {
+                System.out.print("nom > ");
+                String nomR = System.console().readLine();
+                System.out.print("ville > ");
+                String villeR = System.console().readLine();
+                System.out.print("id > ");
+                int idR = Integer.parseInt(System.console().readLine());
+
+                Magasin magasin = new Magasin(nomR, villeR,idR);
+                entreprise.ajouterMagasin(magasin);
+            } else if (commande.equals("p")) {
+                menu3 = true;
             } else {
                 System.out.println("Commande invalide.");
             }
@@ -119,16 +164,13 @@ public class AppLibrairie {
 
     public void auRevoir() {
         System.out.println("╭─────────────────────────────────────────────────╮");
-        System.out.println("│ Au revoir, bonne route et bonne continuation ! │");
+        System.out.println("│ Au revoir, bonne route et bonne continuation !  │");
         System.out.println("╰─────────────────────────────────────────────────╯");
     }
 
     private String lireCommande() {
         System.out.print("Commande > ");
-        String mess = System.console().readLine();
-        String mess2 = mess.strip().toLowerCase();
-        return mess2;
+        String mess = System.console().readLine().strip().toLowerCase();
+        return mess;
     }
-
 }
-
