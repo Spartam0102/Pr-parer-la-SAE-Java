2fb67185ca19d7bfa8561a4ea3f447bbfff22673
Spartam0102 <lubinantonin45@gmail.com>
2025-05-09 13:49:24 +0200

    Remplissage de certaine méthodes

--- src/Editeur.java
+++ src/Editeur.java
@@ -1,38 +1,38 @@
 public class Editeur {
 
     private int idEdit;
-    private String nomEdit; 
-    
-    public Editeur (int idEdit, String nomEdit){
+    private String nomEdit;
+
+    public Editeur(int idEdit, String nomEdit) {
         this.idEdit = idEdit;
         this.nomEdit = nomEdit;
     }
 
-    public int getIdEdit(){
+    public int getIdEdit() {
         return this.idEdit;
     }
 
-    public String getNomEdit(){
+    public String getNomEdit() {
         return this.nomEdit;
     }
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj){
+        if (this == obj) {
             return true;
         }
-        if (obj == null){
+        if (obj == null) {
             return false;
         }
-        if(!(obj instanceof Editeur)){
+        if (!(obj instanceof Editeur)) {
             return false;
         }
         Editeur tmp = (Editeur) obj;
         return this.nomEdit.equals(tmp.nomEdit) && this.idEdit == tmp.idEdit;
     }
-    
+
     @Override
-    public String toString(){
-        return "L'éditeur " + this.nomEdit +" possède l'id " + this.idEdit;
+    public String toString() {
+        return "L'éditeur " + this.nomEdit + " possède l'id " + this.idEdit;
     }
 }
