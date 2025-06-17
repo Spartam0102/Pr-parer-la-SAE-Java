68d0cea74c852a2b4dcc74c4bfca5ca8de81f52a
Morain Nolan <o22403732@or-iut-i205-f04.iut45.univ-orleans.fr>
2025-06-17 11:11:01 +0200

    realisation de la vue de la fenetreMagasins

--- compileIHM.sh
+++ compileIHM.sh
@@ -9,4 +9,4 @@ fi
 echo "✓ Compilation réussie"
 
 echo "Lancement de l'application..."
-java --module-path "/usr/share/openjfx/lib/" --add-modules javafx.controls,javafx.fxml -cp bin IHM.FenetreConnexion
+java --module-path "/usr/share/openjfx/lib/" --add-modules javafx.controls,javafx.fxml -cp bin IHM.FenetreMagasins
